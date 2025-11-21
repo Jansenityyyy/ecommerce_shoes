7ac3750587001dda648560e0d5c55ae9ac9fead7
Sen <jansensilao04@gmail.com>
2025-11-21 15:13:30 +0800

    tite

--- php/fetch_products.php
+++ php/fetch_products.php
@@ -1,33 +1,28 @@
 <?php
 include 'connect.php';
 
-// List of allowed brands (tables)
 $allowed_brands = ['nike', 'adidas', 'puma'];
 
-// Get brand from query, default to all if not set
 $brand = isset($_GET['brand']) ? strtolower($_GET['brand']) : 'all';
 
-// Initialize products array
 $products = [];
 
-// Function to fetch products from a table safely
 function fetchBrandProducts($conn, $table) {
-    // Ensure table name is safe
     $table = mysqli_real_escape_string($conn, $table);
     $sql = "SELECT * FROM `$table`";
     $result = mysqli_query($conn, $sql);
     $rows = [];
+
     if($result){
         while($row = mysqli_fetch_assoc($result)){
-            // Format price as PHP string with 2 decimals
-            $row['price'] = number_format($row['price'], 2, 2, 2, '.', ',');
+            // Keep raw number (avoid formatting here)
+            $row['price'] = floatval($row['price']);
             $rows[] = $row;
         }
     }
     return $rows;
 }
 
-// Fetch products based on brand
 if($brand != 'all' && in_array($brand, $allowed_brands)){
     $products = fetchBrandProducts($conn, $brand);
 } else {
@@ -36,9 +31,7 @@ if($brand != 'all' && in_array($brand, $allowed_brands)){
     }
 }
 
-// Return JSON
 echo json_encode($products, JSON_UNESCAPED_UNICODE);
 
-// Close connection
 mysqli_close($conn);
 ?>
